e453c0d54757936809710719fd794f36b24a0dcd
Svat-dev <svatoslavgetun@gmail.com>
2025-06-17 12:08:36 +0300

    Added Bot With Commands

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,9 +14,20 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
-@dp.message()
-async def echo_func(message: types.Message):
-    await message.answer(message.text)
+@dp.message(Command("telljoke"))
+async def tell_joke(message: types.Message):
+    joke = "Колобок повесился"
+    message.answer(joke)
+
+@dp.message(Command("getweather"))
+async def get_weather(message: types.Message):
+    weather = "Сейчас солнечно! Осадков не ожидется"
+    message.answer(weather)
+
+@dp.message(Command("getdate"))
+async def get_date(message: types.Message):
+    date = f"Сейчас: {datetime.now().strftime('%H:%M:%S')}"
+    message.answer(date)
 
 async def main():
     await dp.start_polling(bot)
