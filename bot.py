e86d41f275508e336664ff4c30ce8131ccce175b
unknown <svatoslavgetun@gmail.com>
2025-06-17 12:50:44 +0300

    first commit

--- bot.py
+++ bot.py
@@ -1,33 +1,49 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
 from datetime import datetime
+from config import config
+from data import kb
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token="7825350159:AAE7xkTmZSgRbxRG18fsdDQ3QO7SHRIDn5o")
+bot = Bot(token=config.bot_token.get_secret_value())
 
 dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите скорость выполнения"
+    )
+
+    await message.answer("Как быстро вы хотите получить IPhone 16 Pro Max?", reply_markup=keyboard)
+
+@dp.message(F.text.lower() == "быстро")
+async def make_it_fast(message: types.Message):
+    await message.reply("С вас дополнительно 10 руб.")
+
+@dp.message(F.text.lower() == "в своем порядке")
+async def make_it_slow(message: types.Message):
+    await message.reply("Вы получите его ровно через год")
 
 @dp.message(Command("telljoke"))
 async def tell_joke(message: types.Message):
     joke = "Колобок повесился"
-    message.answer(joke)
+    await message.answer(joke)
 
 @dp.message(Command("getweather"))
 async def get_weather(message: types.Message):
     weather = "Сейчас солнечно! Осадков не ожидется"
-    message.answer(weather)
+    await message.answer(weather)
 
 @dp.message(Command("getdate"))
 async def get_date(message: types.Message):
     date = f"Сейчас: {datetime.now().strftime('%H:%M:%S')}"
-    message.answer(date)
+    await message.answer(date)
 
 async def main():
     await dp.start_polling(bot)
